59d28aee4a725605cca416f96f265ad306e7c2d8
Thao Nguyen <thaobeo10122000@gmail.com>
2024-07-17 02:58:18 +0200

    operator

--- scripts/dist/operation.dev.js
+++ scripts/dist/operation.dev.js
@@ -4,10 +4,39 @@
 {
   var total = 10 + 7; // 17
 
-  var n = 100 % 3; // 1 
+  var n = 100 % 3; // 1
 
-  var number = 5;
-  number = number + 10; //number += 10; // 15
-  //console.log(number++); // 15
-  //console.log(++number); //  16
+  var number = 10;
+  number = number + 10; //number += 10; // 110
+  // console.log(number++); // 110 (gán trước, tăng giá trị sau)
+  //console.log(number); // 16
+  //console.log(++number); //  16 (tăng trước, gán giá trị sau)
 } // 2. Comparison Operators:  > < >= <= == ===
+
+{
+  console.log(10 > 7);
+  console.log(10 < 7);
+  console.log(10 >= 7);
+  console.log(10 <= 7);
+  console.log("7" == "7"); // true
+
+  console.log(null == undefined); // true
+
+  console.log(null == 10); // false
+
+  console.log(1 == true); // true
+
+  console.log(5 == "5"); // Number("5") -> 5
+} // 3. Logical Operators: && || !
+
+{
+  console.log(!false); // true
+
+  console.log(1 && 2 && true && 10); // 10 (code chạy từ trái qua phải, nếu tất cả là true -> trả về kết quả cuối cùng)
+
+  console.log(1 && 2 && false && 10); // false (nếu gặp false -> dừng lại và trả về false)
+
+  console.log(1 || 2 || 3); // 1 (gặp true đầu tiên sẽ dừng lại và trả về giá trị đó)
+
+  console.log(false || false || true); // true 
+}
